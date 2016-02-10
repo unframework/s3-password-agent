8abee89a022d32669608af1c19b20e80e46b617a
Nick Matantsev <nick.matantsev@gmail.com>
2016-02-10 00:28:46 -0500

    Added basic link replacement in client

--- client.js
+++ client.js
@@ -1,5 +1,7 @@
 (function (window, document) {
+    // @todo fill console object as needed
     var LINK_AGENT_ROUTE = '/s3-link-agent.js';
+    var GO_ROUTE_PREFIX = '/go/';
 
     function detectBaseURLPrefix() {
         var fullScriptList = Array.prototype.slice.call(document.querySelectorAll('script'));
@@ -14,11 +16,31 @@
         return selfScriptList[0].src.slice(0, -LINK_AGENT_ROUTE.length);
     }
 
-    // detect source
-    var baseURLPrefix = detectBaseURLPrefix();
-    console.log('got prefix', baseURLPrefix);
+    function convertLink(linkDom, baseURLPrefix) {
+        // get href as declared in page source, not computed href property
+        var href = linkDom.getAttribute('href');
 
-    var linkList = document.querySelectorAll('a');
-    console.log('got links', linkList);
+        // look for specially marked hrefs
+        if (!href || href.slice(0, 4) !== '#s3/') {
+            return;
+        }
 
+        linkDom.href = baseURLPrefix + GO_ROUTE_PREFIX + href.slice(4);
+    }
+
+    function main() {
+        // detect source (our own script tag should be available immediately)
+        var baseURLPrefix = detectBaseURLPrefix();
+        console.log('s3-link-agent: URL prefix =', baseURLPrefix);
+
+        window.addEventListener('load', function () {
+            var linkList = Array.prototype.slice.call(document.querySelectorAll('a'));
+
+            linkList.forEach(function (linkDom) {
+                convertLink(linkDom, baseURLPrefix);
+            });
+        }, false);
+    }
+
+    main();
 })(window, document);
