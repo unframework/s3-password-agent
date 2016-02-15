681ef003d395811298cfdbac57cd8da9bd052597
Nick Matantsev <nick.matantsev@gmail.com>
2016-02-14 19:11:19 -0500

    Added basic PIN user validation

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ var AWS = require('aws-sdk');
 var express = require('express');
 var cors = require('cors');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var yaml = require('js-yaml');
 
 var S3_BUCKET = process.env.S3_BUCKET;
 var CONTENT_CONFIG_FILE = __dirname + '/content.yaml';
+var USERS_CONFIG_FILE = __dirname + '/users.yaml';
 
 var AUTH_COOKIE = 's3-link-agent-93f04cb9-f0a0-475d-8c86-cf610c2002b5';
 var LINK_AGENT_ROUTE = '/s3-link-agent.js';
 
 var contentYaml = Array.prototype.slice.call(yaml.safeLoad(fs.readFileSync(CONTENT_CONFIG_FILE)));
+var usersYaml = yaml.safeLoad(fs.readFileSync(USERS_CONFIG_FILE));
 
 // whitelist accessible keys
 var objectKeyMap = Object.create(null);
@@ -28,6 +31,7 @@ contentYaml.forEach(function (rawConfigValue) {
 });
 
 console.log('using whitelist:', Object.keys(objectKeyMap));
+console.log('authorized users list:', Object.keys(usersYaml));
 
 var origin = 'http://localhost:3000';
 
@@ -139,7 +143,29 @@ sessionApp.use(function (req, res, next) {
 });
 sessionApp.use(cookieParser());
 
-sessionApp.post('', function (req, res) {
+sessionApp.post('', bodyParser.json(), function (req, res) {
+    var email = req.body.email;
+    var pin = req.body.pin;
+
+    // this is super cheesy, so only numeric pins are allowed,
+    // to avoid semblance of any real security
+    // although, gosh, people might type in their bank digits... @todo secure this
+    if (!/^[0-9]{4,6}$/.exec(pin)) {
+        res.status(400);
+        res.send('malformed pin');
+        return;
+    }
+
+    // validate credentials in a super cheesy way
+    var authInfo = usersYaml.hasOwnProperty(email) ? usersYaml[email] : null;
+
+    if (!authInfo || authInfo.pin + '' !== pin) {
+        res.status(403);
+        res.send('not authorized');
+        return;
+    }
+
+    // user is authenticated
     var sessionKey = '_' + Math.random();
 
     setTimeout(function () {
