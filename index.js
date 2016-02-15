e1689dc57d922deb9464e410c2f37ac0cf7d5d6e
Nick Matantsev <nick.matantsev@gmail.com>
2016-02-15 14:51:20 -0500

    Encapsulated client route

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 var fs = require('fs');
-var stream = require('stream');
-var browserify = require('browserify');
-var Promise = require('bluebird');
 var AWS = require('aws-sdk');
 var express = require('express');
 
 var SessionMiddleware = require('./lib/SessionMiddleware');
+var ClientAssetRouter = require('./lib/ClientAssetRouter');
 var LinkRouter = require('./lib/LinkRouter');
 var SessionRouter = require('./lib/SessionRouter');
 
@@ -24,49 +22,8 @@ var origin = 'http://localhost:3000'; // @todo configure
 var s3 = new AWS.S3();
 var sessionMiddleware = new SessionMiddleware(AUTH_COOKIE);
 
-function whenClientSideCodeReady(sourceCode) {
-    var clientRC = new stream.Readable();
-    clientRC._read = function () {};
-    clientRC.push(sourceCode);
-    clientRC.push(null);
-
-    var b = browserify().add(clientRC, { basedir: __dirname });
-
-    return new Promise(function (resolve, reject) {
-        b.bundle(function (err, buffer) {
-            if(err) {
-                reject(err);
-            } else {
-                resolve(buffer);
-            }
-        });
-    });
-}
-
 var app = express();
-
-// @todo rate limiting
-app.get(LINK_AGENT_ROUTE, function (req, res) {
-    // grab the client code file
-    // @todo minify/etc?
-    // @todo cache the code?
-    fs.readFile('client.js', function (err, fileData) {
-        if (err) {
-            console.error('could not read client file', err);
-
-            res.status(500);
-            res.send('error serving client');
-            return;
-        }
-
-        whenClientSideCodeReady(fileData).then(function (output) {
-            res.setHeader('Content-Type', 'application/javascript');
-            res.send(output);
-        });
-    });
-});
-
+app.use(LINK_AGENT_ROUTE, new ClientAssetRouter(__dirname));
 app.use('/go', new LinkRouter(s3, S3_BUCKET, contentYamlData, sessionMiddleware));
 app.use('/session', new SessionRouter(origin, usersYamlData, sessionMiddleware));
-
 app.listen(process.env.PORT || 3000);
