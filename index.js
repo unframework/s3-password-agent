6a27aaebe2161c6911c9269e579e7dfe98f273f4
Nick Matantsev <nick.matantsev@gmail.com>
2016-02-15 14:42:25 -0500

    Encapsulated link signer route

--- index.js
+++ index.js
@@ -4,10 +4,9 @@ var browserify = require('browserify');
 var Promise = require('bluebird');
 var AWS = require('aws-sdk');
 var express = require('express');
-var cookieParser = require('cookie-parser');
-var yaml = require('js-yaml');
 
 var SessionMiddleware = require('./lib/SessionMiddleware');
+var LinkRouter = require('./lib/LinkRouter');
 var SessionRouter = require('./lib/SessionRouter');
 
 var S3_BUCKET = process.env.S3_BUCKET;
@@ -17,21 +16,8 @@ var USERS_CONFIG_FILE = __dirname + '/users.yaml';
 var AUTH_COOKIE = 's3-link-agent-93f04cb9-f0a0-475d-8c86-cf610c2002b5';
 var LINK_AGENT_ROUTE = '/s3-link-agent.js';
 
-var contentYaml = Array.prototype.slice.call(yaml.safeLoad(fs.readFileSync(CONTENT_CONFIG_FILE)));
+var contentYamlData = fs.readFileSync(CONTENT_CONFIG_FILE);
 var usersYamlData = fs.readFileSync(USERS_CONFIG_FILE);
-
-// whitelist accessible keys
-var objectKeyMap = Object.create(null);
-contentYaml.forEach(function (rawConfigValue) {
-    if (typeof rawConfigValue !== 'string') {
-        throw new Error('content key whitelist must contain only strings');
-    }
-
-    var key = rawConfigValue[0] === '/' ? rawConfigValue.substring(1) : rawConfigValue;
-    objectKeyMap[key] = true;
-});
-
-console.info('using whitelist:', Object.keys(objectKeyMap));
 
 var origin = 'http://localhost:3000'; // @todo configure
 
@@ -80,42 +66,7 @@ app.get(LINK_AGENT_ROUTE, function (req, res) {
     });
 });
 
-// @todo rate limiting
-app.get(/^\/go\/(.*)$/, cookieParser(), sessionMiddleware, function (req, res) {
-    var user = req.session.user;
-    var unsafeObjectPath = req.params[0];
-
-    // check whitelist
-    if (!Object.prototype.hasOwnProperty.call(objectKeyMap, unsafeObjectPath)) {
-        console.error('unknown key requested', unsafeObjectPath);
-
-        res.status(500)
-        res.send('cannot redirect');
-        return;
-    }
-
-    var accessibleObjectKey = unsafeObjectPath;
-
-    var params = {
-        Key: accessibleObjectKey,
-        Bucket: S3_BUCKET,
-        Expires: 60 // 1 minute
-    };
-
-    s3.getSignedUrl('getObject', params, function (err, url) {
-        if (err) {
-            console.error('could not get url', err);
-
-            res.status(500)
-            res.send('cannot redirect');
-            return;
-        }
-
-        console.info('redirecting', user, 'to:', url); // @todo more info or just remove
-        res.redirect(302, url);
-    });
-});
-
+app.use('/go', new LinkRouter(s3, S3_BUCKET, contentYamlData, sessionMiddleware));
 app.use('/session', new SessionRouter(origin, usersYamlData, sessionMiddleware));
 
 app.listen(process.env.PORT || 3000);
