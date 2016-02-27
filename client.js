7ade5c95c9ba2e6caa06a6a91bffd45edd03761b
Nick Matantsev <nick.matantsev@gmail.com>
2016-02-27 00:13:21 -0500

    Carried out reusable widget code

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ var vdomLive = require('vdom-live');
 
 var Auth = require('./lib/Auth');
 var AuthWallView = require('./lib/AuthWallView');
+var ClientWidget = require('./lib/ClientWidget');
 
 // @todo fill console object as needed
 
@@ -9,56 +10,21 @@ var AuthWallView = require('./lib/AuthWallView');
 var LINK_AGENT_ROUTE = '/s3-link-agent.js';
 var GO_ROUTE_PREFIX = '/go/';
 
-function findScriptBySrcSuffix(suffix) {
-    var fullScriptList = Array.prototype.slice.call(document.querySelectorAll('script'));
-    var selfScriptList = fullScriptList.filter(function (dom) {
-        return (dom.src && dom.src.slice(-suffix.length) === suffix);
-    });
-
-    if (selfScriptList.length !== 1) {
-        throw new Error('expected to find just one script matching self');
-    }
-
-    return selfScriptList[0];
-}
-
-function convertLink(linkDom, baseURLPrefix) {
-    // get href as declared in page source, not computed href property
-    var href = linkDom.getAttribute('href');
-
-    // look for specially marked hrefs
-    if (!href || href.slice(0, 4) !== '#s3/') {
-        return;
-    }
-
-    linkDom.href = baseURLPrefix + GO_ROUTE_PREFIX + href.slice(4);
-}
-
 function main() {
-    // detect source (our own script tag should be available immediately)
-    var baseURLPrefix = findScriptBySrcSuffix(LINK_AGENT_ROUTE).src.slice(0, -LINK_AGENT_ROUTE.length);
-    console.log('s3-link-agent: URL prefix =', baseURLPrefix);
+    new ClientWidget(LINK_AGENT_ROUTE, GO_ROUTE_PREFIX, function (baseURLPrefix) {
+        var rootNode = null;
 
-    var rootNode = null;
+        vdomLive(function (renderLive, h) {
+            var auth = new Auth(baseURLPrefix);
 
-    vdomLive(function (renderLive, h) {
-        var auth = new Auth(baseURLPrefix);
-
-        var authView = new AuthWallView(auth, h);
-        rootNode = renderLive(function () {
-            return authView.render();
+            var authView = new AuthWallView(auth, h);
+            rootNode = renderLive(function () {
+                return authView.render();
+            });
         });
+
+        return rootNode;
     });
-
-    window.addEventListener('DOMContentLoaded', function () {
-        document.body.appendChild(rootNode);
-
-        var linkList = Array.prototype.slice.call(document.querySelectorAll('a'));
-
-        linkList.forEach(function (linkDom) {
-            convertLink(linkDom, baseURLPrefix);
-        });
-    }, false);
 }
 
 main();
