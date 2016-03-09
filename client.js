525d8574fcdc9bc12c47cdef7989b0831644124d
Nick Matantsev <nick.matantsev@gmail.com>
2016-03-08 22:52:29 -0500

    Made client widget class more reusable

--- client.js
+++ client.js
@@ -10,8 +10,20 @@ var ClientWidget = require('./lib/ClientWidget');
 var LINK_AGENT_ROUTE = '/s3-link-agent.js';
 var GO_ROUTE_PREFIX = '/go/';
 
+function convertLink(linkDom, prefix) {
+    // get href as declared in page source, not computed href property
+    var href = linkDom.getAttribute('href');
+
+    // look for specially marked hrefs
+    if (!href || href.slice(0, 4) !== '#s3/') {
+        return;
+    }
+
+    linkDom.href = prefix + href.slice(4);
+}
+
 function main() {
-    new ClientWidget(LINK_AGENT_ROUTE, GO_ROUTE_PREFIX, function (baseURLPrefix) {
+    new ClientWidget(LINK_AGENT_ROUTE, function (baseURLPrefix) {
         var rootNode = null;
 
         vdomLive(function (renderLive, h) {
@@ -24,6 +36,14 @@ function main() {
         });
 
         return rootNode;
+    }, function (baseURLPrefix, rootNode) {
+        document.body.appendChild(rootNode);
+
+        var linkList = Array.prototype.slice.call(document.querySelectorAll('a'));
+
+        linkList.forEach(function (linkDom) {
+            convertLink(linkDom, baseURLPrefix + GO_ROUTE_PREFIX);
+        });
     });
 }
 
